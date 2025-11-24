0f747679cd4c6227321bf6c2816eb9798bcbe04d
Paulo Santos <santospaulor21@gmail.com>
2025-11-24 10:55:41 -0300

    feat:  linked_list_merge_sort

--- linked_list.py
+++ linked_list.py
@@ -108,6 +108,19 @@ class LinkedList:
             prev.next_node = new_node
             new_node.next_node = next_n
 
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position +=1
+
+            return current
+
     def remove(self, key):
         """
         Removes Node containing data that matches
